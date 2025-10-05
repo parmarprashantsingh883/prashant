da3eaee6171972503017eff38bed24852bc45b7c
parmar prashant <parmarprashantsingh883@gmail.com>
2025-10-06 00:05:05 +0530

    first commit

---
+++ js/my.js
@@ -0,0 +1,11 @@
+let val1=10;
+let val2=20;
+function addnum(...res){
+    let total=a+b
+    console.log(total);
+    
+
+}
+// let result1=addnum(val1,val2)
+// let result2=addnum(10,52)
+// console.log(result2);
