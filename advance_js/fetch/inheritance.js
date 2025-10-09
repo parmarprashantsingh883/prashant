4acf4f3697ffd8c5f7b69745384c39f758e24b9e
parmar prashant <parmarprashantsingh883@gmail.com>
2025-10-09 14:41:56 +0530

    first commit

--- advance_js/fetch/inheritance.js
+++ advance_js/fetch/inheritance.js
@@ -1,12 +1,25 @@
-class ParentComponent extends React.Component {
-  parentMethod() {
-    console.log("Parent method");
-  }
+// class ParentComponent extends React.Component {
+//   parentMethod() {
+//     console.log("Parent method");
+//   }
+// }
+
+// class ChildComponent extends ParentComponent {
+//   render() {
+//     this.parentMethod(); // can call parent method
+//     return <h1>Child</h1>;
+//   }
+// }
+
+function greet(name) {
+  console.log("Hello " + name);
 }
 
-class ChildComponent extends ParentComponent {
-  render() {
-    this.parentMethod(); // can call parent method
-    return <h1>Child</h1>;
-  }
+function processUser(callback) {
+  let userName = "Prashant";
+  callback(userName); // Call the callback function
 }
+
+processUser(greet); // Output: Hello Prashant
+
+
