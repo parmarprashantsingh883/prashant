0edcdb047bb1672c66b04c42855579adb2b82544
parmar prashant <parmarprashantsingh883@gmail.com>
2025-11-05 22:27:04 +0530

    first commit

---
+++ React_apps/practice/src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  const num = 5;
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-400">
+        Hello World now me addiing 5+5 {5+5}
+      </h1>
+    </>
+  );
+};
+
+export default App;
