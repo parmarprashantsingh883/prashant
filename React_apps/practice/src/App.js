8c34abcda503797c2c1c77505c09580a1d2937ff
parmar prashant <parmarprashantsingh883@gmail.com>
2025-11-08 09:05:11 +0530

    first commit

--- React_apps/practice/src/App.js
+++ React_apps/practice/src/App.js
@@ -5,24 +5,7 @@ const App = () => {
   return (
     <>
     <div className="parent">
-      <Card 
-      title="laptop"
-      description="Powerful laptop with 16GB RAM and 512GB SSD."
-       buttonText="Buy Now"
-       img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiuaWqDZxP5nbQaFnugT06LWjBaI_50XUrfA&s"
-       />
-        <Card 
-      title="phone"
-      description="Powerful phone with 8GB RAM and 128GB Storage."
-       buttonText="Buy Now"
-       img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA3agUX-jsrVyL4_-JOESngaNZNFxvmWzpWg&s"
-       />
-       <Card 
-      title="phone"
-      description="Powerful phone with 8GB RAM and 128GB Storage."
-       buttonText="Buy Now"
-       img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA3agUX-jsrVyL4_-JOESngaNZNFxvmWzpWg&s"
-       />
+      <Card />
      
     </div>
       </>
--- React_apps/practice/src/components/Card.js
+++ React_apps/practice/src/components/Card.js
@@ -1,18 +1,31 @@
-import React from 'react'
-
+import { Bookmark } from 'lucide-react'
 const Card = (props) => {
-  return (
-   <>
- <div className="card">
-    <img src={props.img}  />
-    <h2>{props.title}</h2>
-    <p>{props.description}</p>
-  <button>{props.buttonText}</button>
 
- </div>
- 
-   </>
-  ) 
+    return (
+        <div className="card">
+            <div>
+                <div className="top">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFkkQhwA-14yQus_rij6VonTP5UT7IrorBkg&s"alt="nachioooooo" />
+                    <button>Save <Bookmark size={10} /> </button>
+                </div>
+                <div className="center">
+                    <h3>Amazon <span>5 days ago</span></h3>
+                    <h2>Senior UI/UX Designer</h2>
+                    <div className='tag'>
+                        <h4>part time </h4>
+                        <h4>senior level</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
+        </div>
+    )
 }
-    
-export default Card;
+
+export default Card
